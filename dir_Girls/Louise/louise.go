4465326eb1eedf247e8811ebc7a7e02dc0bcd8f6
Arfikait Sirius <arfikait.sirius@gmail.com>
2021-10-05 20:54:27 +0900

    Learn Louise. - FnLowerAll() and FnUpperFirst().

--- dir_Girls/Louise/louise.go
+++ dir_Girls/Louise/louise.go
@@ -8,6 +8,38 @@ type louise struct{}
 
 var Louise louise
 
+//------------------------
+// :[ NAME ]:
+//     FnUpperAll
+//
+// :[ CATEGORY ]:
+//     Skill
+//------------------------
 func ( l louise ) FnUpperAll( base string ) string {
 	return strings.ToUpper( base )
+}
+
+//------------------------
+// :[ NAME ]:
+//     FnLowerAll
+//
+// :[ CATEGORY ]:
+//     Skill
+//------------------------
+func ( l louise ) FnLowerAll( base string ) string {
+	return strings.ToLower( base )
+}
+
+//------------------------
+// :[ NAME ]:
+//     FnUpperFirst
+//
+// :[ CATEGORY ]:
+//     Skill
+//------------------------
+func ( l louise ) FnUpperFirst( base string ) string {
+	upper := strings.ToUpper( base )
+	lower := strings.ToLower( base )
+
+	return upper[:1] + lower[1:]
 }
